2244019f2da5d44384558135f7009d1cb2deec5c
RockerHX <RockerHX@gmail.com>
2015-11-17 00:35:25 +0800

    更新：
    1.注册登录模块文件结构
    2.完成注册服务条款页面以及逻辑

---
+++ TipTop-User/ViewControllers/Login/Register/HXServiceTermsViewController.m
@@ -0,0 +1,29 @@
+//
+//  HXServiceTermsViewController.m
+//  TipTop-User
+//
+//  Created by ShiCang on 15/11/17.
+//  Copyright © 2015年 Outsourcing. All rights reserved.
+//
+
+#import "HXServiceTermsViewController.h"
+#import "HXApi.h"
+
+@interface HXServiceTermsViewController ()
+@end
+
+@implementation HXServiceTermsViewController
+
+#pragma mark - View Controller Life Cycle
+- (void)viewDidLoad {
+    self.loadURL = [DoMain stringByAppendingFormat:@"/h5/page?key=client_term"];
+    
+    [super viewDidLoad];
+}
+
+#pragma mark - Setter And Getter
+- (HXStoryBoardName)storyBoardName {
+    return HXStoryBoardNameLogin;
+}
+
+@end
