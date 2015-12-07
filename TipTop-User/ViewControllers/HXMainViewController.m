7344d2895b5ce18e151dadb3357e066588beaacd
RockerHX <RockerHX@gmail.com>
2015-12-08 01:47:03 +0800

    Build4:
    1.完善部分界面
    2.修复登录跳转问题

--- TipTop-User/ViewControllers/HXMainViewController.m
+++ TipTop-User/ViewControllers/HXMainViewController.m
@@ -77,16 +77,13 @@
 
 #pragma mark - HXLoginViewControllerDelegate Methods
 - (void)loginViewControllerLoginSuccess:(HXLoginViewController *)loginViewController {
-    UIView *view = [self.contentViewController.view snapshotViewAfterScreenUpdates:YES];
+    UIView *view = nil;
     __weak __typeof__(self)weakSelf = self;
-    [UIView transitionFromView:loginViewController.view toView:view duration:1.0f options:UIViewAnimationOptionTransitionFlipFromLeft completion:
-     ^(BOOL finished) {
+    [UIView transitionFromView:loginViewController.view toView:view duration:1.0f options:UIViewAnimationOptionTransitionFlipFromLeft completion:^(BOOL finished) {
         __strong __typeof__(self)strongSelf = weakSelf;
         [loginViewController.view removeFromSuperview];
         [loginViewController removeFromParentViewController];
-        [view removeFromSuperview];
-         
-         [strongSelf showHomePageViewController];
+        [strongSelf showHomePageViewController];
     }];
 }
 
