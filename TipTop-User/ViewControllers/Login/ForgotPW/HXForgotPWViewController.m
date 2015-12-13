3ee84d7a341fdd1f5e021598870414519b94f233
RockerHX <RockerHX@gmail.com>
2015-12-14 00:22:41 +0800

    修复：
    1.找回密码时各项信息未验证提示的问题

--- TipTop-User/ViewControllers/Login/ForgotPW/HXForgotPWViewController.m
+++ TipTop-User/ViewControllers/Login/ForgotPW/HXForgotPWViewController.m
@@ -48,15 +48,18 @@ static NSString *ForgotPassWordApi = @"/session/forgotPwd";
 }
 
 - (IBAction)enterButtonPressed {
-    if (_mobileTextField.text.length &&
-        _captchaTextField.text.length &&
-        _passWordTextField.text.length &&
-        _confirmTextField.text.length) {
-        if ([_confirmTextField.text isEqualToString:_passWordTextField.text]) {
-            [self startForgotPassWordRequestWithParameters:@{@"mobile": _mobileTextField.text,
-                                                            @"captcha": _captchaTextField.text,
-                                                           @"password": _passWordTextField.text}];
-        }
+    if (_mobileTextField.text.length != 11) {
+        [self showAlertWithMessage:@"请输入正确手机号！"];
+    } else if (_captchaTextField.text.length != 4) {
+        [self showAlertWithMessage:@"请输入正确验证码！"];
+    } else if (!_passWordTextField.text.length) {
+        [self showAlertWithMessage:@"请输入注册密码！"];
+    } else if (![_passWordTextField.text isEqualToString:_confirmTextField.text]) {
+        [self showAlertWithMessage:@"亲，您输入的两次密码不相同噢！"];
+    } else {
+        [self startForgotPassWordRequestWithParameters:@{@"mobile": _mobileTextField.text,
+                                                         @"captcha": _captchaTextField.text,
+                                                         @"password": _passWordTextField.text}];
     }
 }
 
@@ -66,15 +69,13 @@ static NSString *ForgotPassWordApi = @"/session/forgotPwd";
     [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:YES];
     [HXAppApiRequest requestPOSTMethodsWithAPI:[HXApi apiURLWithApi:CaptchaApi] parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
         __strong __typeof__(self)strongSelf = weakSelf;
+        [MBProgressHUD hideHUDForView:strongSelf.navigationController.view animated:YES];
         NSInteger errorCode = [responseObject[@"error_code"] integerValue];
         if (HXAppApiRequestErrorCodeNoError == errorCode) {
-            [UIAlertView bk_showAlertViewWithTitle:@"发送成功！"
-                                           message:nil
-                                 cancelButtonTitle:@"确定"
-                                 otherButtonTitles:nil
-                                           handler:nil];
+            [self showAlertWithMessage:@"发送成功！"];
+        } else {
+            [self showAlertWithMessage:responseObject[@"tip"]];
         }
-        [MBProgressHUD hideHUDForView:strongSelf.navigationController.view animated:YES];
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         __strong __typeof__(self)strongSelf = weakSelf;
         [MBProgressHUD hideHUDForView:strongSelf.navigationController.view animated:YES];
@@ -85,15 +86,13 @@ static NSString *ForgotPassWordApi = @"/session/forgotPwd";
     [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:YES];
     [HXAppApiRequest requestPOSTMethodsWithAPI:[HXApi apiURLWithApi:ForgotPassWordApi] parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
         __strong __typeof__(self)strongSelf = weakSelf;
+        [MBProgressHUD hideHUDForView:strongSelf.navigationController.view animated:YES];
         NSInteger errorCode = [responseObject[@"error_code"] integerValue];
         if (HXAppApiRequestErrorCodeNoError == errorCode) {
-            [UIAlertView bk_showAlertViewWithTitle:@"重置成功！"
-                                           message:nil
-                                 cancelButtonTitle:@"确定"
-                                 otherButtonTitles:nil
-                                           handler:nil];
+            [self showAlertWithMessage:@"重置成功！"];
+        } else {
+            [self showAlertWithMessage:responseObject[@"tip"]];
         }
-        [MBProgressHUD hideHUDForView:strongSelf.navigationController.view animated:YES];
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         __strong __typeof__(self)strongSelf = weakSelf;
         [MBProgressHUD hideHUDForView:strongSelf.navigationController.view animated:YES];
