15ef2569523ff10bf06ddfbb88dd852b060626e7
RockerHX <RockerHX@gmail.com>
2016-03-13 20:02:00 +0800

    Build:9

--- TipTop-User/ViewControllers/User/Setting/HXFeedBackViewController.m
+++ TipTop-User/ViewControllers/User/Setting/HXFeedBackViewController.m
@@ -1,7 +1,6 @@
 //
 //  HXFeedBackViewController.m
-
-//  TipTop-User
+//  TipTop-Adviser
 //
 //  Created by ShiCang on 15/11/17.
 //  Copyright © 2015年 Outsourcing. All rights reserved.
@@ -9,17 +8,37 @@
 
 #import "HXFeedBackViewController.h"
 #import "HXApi.h"
+#import "WebViewJavascriptBridge.h"
+#import <MBProgressHUD/MBProgressHUD.h>
+#import "UIAlertView+BlocksKit.h"
 
-@interface HXFeedBackViewController ()
+@interface HXFeedBackViewController () <UIWebViewDelegate>
 @end
 
-@implementation HXFeedBackViewController
+@implementation HXFeedBackViewController {
+    WebViewJavascriptBridge *_bridge;
+}
 
 #pragma mark - View Controller Life Cycle
 - (void)viewDidLoad {
-    self.loadURL = [HXApi webViewURLWithURL:@"/h5/feedback?app=client"];
+    [self initConfigure];
     
     [super viewDidLoad];
+}
+
+#pragma mark - Private Methods
+- (void)initConfigure {
+    self.loadURL = [HXApi webViewURLWithURL:@"/h5/feedback?app=client"];
+}
+
+- (void)loadWebView {
+    [super loadWebView];
+    
+    __weak __typeof__(self)weakSelf = self;
+    _bridge = [WebViewJavascriptBridge bridgeForWebView:self.webView webViewDelegate:self handler:^(id data, WVJBResponseCallback responseCallback) {
+        __strong __typeof__(self)strongSelf = weakSelf;
+        [strongSelf handleJSData:data];
+    }];
 }
 
 #pragma mark - Setter And Getter
@@ -30,6 +49,44 @@
 #pragma mark - Setter And Getter
 - (HXStoryBoardName)storyBoardName {
     return HXStoryBoardNameSetting;
+}
+
+#pragma mark - Web View Delegate Methods
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    [self hideHUD];
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    [self hideHUD];
+}
+
+#pragma mark - Private Methods
+- (void)handleJSData:(id)data {
+    NSLog(@"%@", data);
+    NSData *jsonData = [data dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *events = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:nil];
+    [self handleEvents:events];
+}
+
+- (void)handleEvents:(NSDictionary *)events {
+    NSString *event = events[@"event"];
+    NSString *message = events[@"params"][@"msg"];
+    if ([event isEqualToString:@"confirm"]) {
+        [UIAlertView bk_showAlertViewWithTitle:message
+                                       message:nil
+                             cancelButtonTitle:@"完结"
+                             otherButtonTitles:@[@"取消"]
+                                       handler:
+         ^(UIAlertView *alertView, NSInteger buttonIndex) {
+             if (buttonIndex == alertView.cancelButtonIndex) {
+                 [_bridge callHandler:@"confirmEnter"];
+             } else {
+                 [_bridge callHandler:@"confirmCancel"];
+             }
+         }];
+    } else if ([event isEqualToString:@"alert"]) {
+        [self showAlertWithMessage:message];
+    }
 }
 
 @end
